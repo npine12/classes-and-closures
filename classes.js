f96daf1392a0f264f5240bb8c55eadaf06d36231
Noe Pinedo <noe@MacBook-Pro.localdomain>
2021-04-23 18:04:16 -0600

    done

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return this.first_name + ' ' + this.last_name + ' ' + 'Widget'
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,13 +58,27 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports = []) {
+    super(first_name, last_name, email, age);
+    this.reports = reports
+  }
+  hire(newEmployee) {
+    this.reports.push(newEmployee)
+  }
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+
+}
 
 
 ////////// PROBLEM 3 //////////
 
 /*
   Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
-  create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a manager with the following additional properties:
+  create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a 
+  manager with the following additional properties:
     - title - default 'Not a manager'
     - bonus - default 0
 
@@ -72,6 +96,43 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(firstName, lastName, email, age, reports, title = 'Not a manager', bonus = 0) {
+    super(firstName, lastName, email, age);
+    this.title = title;
+    this.bonus = bonus
+  }
+  fire(index) {
+    super.fire(index);
+    this.bonus += 100
+  }
+  hire(employee) {
+    super.hire(employee);
+    let length = this.reports.length
+    if (length === 0) {
+      this.title = 'Not a manager'
+    } else if (length <= 3) {
+      this.title = 'Barely Manager'
+    } else if (length <= 10) {
+      this.title = 'Mostly Manager'
+    } else if (length <= 50) {
+      this.title = 'Manager'
+    } else if (length <= 100) {
+      this.title = 'Manager Plus'
+    } else {
+      this.title = 'Bestest Manager'
+    }
+    // switch (length) {
+    //   case 0:
+    //     this.title = 'Not a manager'
+    //     break
+    //   case 4: this.title = 'Barely Manager'
+    //     break
+    //   default:
+    // }
+  }
+
+}
 
 
 
@@ -99,5 +160,24 @@
 */
 
 //Code Here
-
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(number) {
+    this.widgets_made_count += number
+    this.wear_and_tear_count += Math.floor(number / 50)
+  }
+  fixMachine() {
+    this.needs_reboot = true
+  }
+  reboot() {
+    return () => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
 
